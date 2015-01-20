f083dad13fa0217b13fa57855dc9ad27cc0a12b7
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-01-20 14:52:34 +0100

    Added dialog to change the feature of the marked code

--- plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/MarkWithFeatureAction.java
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/MarkWithFeatureAction.java
@@ -1,108 +1,77 @@
 package de.ovgu.featureide.core.cide;
 
-import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ISelection;
-
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.dialogs.ListDialog;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.ITextEditor;
 
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import de.ovgu.featureide.core.CorePlugin;
+import de.ovgu.featureide.core.IFeatureProject;
 
 //import de.ovgu.featureide.ui.editors.annotation.ColorAnnotationModel;
 
 public class MarkWithFeatureAction implements IEditorActionDelegate,
 		IViewActionDelegate {
 
-	private ITextEditor activeEditor = null;
-	private boolean highlighting = true;
+	protected List<String> featureNames = new LinkedList<String>();
+
+	public ITextEditor activeEditor = null;
+	Shell parentShel = null;
+	ListDialog listDialog = new ListDialog(parentShel);
 
 	public void run(IAction action) {
-		/*
-		 * highlighting = !highlighting; if (activeEditor != null) {
-		 * ColorAnnotationModel.setHighlighting(highlighting, activeEditor); }
-		 */
+		
+		ColorAnnotationManager colorAnnotationManager = new ColorAnnotationManager(); 
+		ISelectionProvider selectionProvider = activeEditor .getSelectionProvider(); 
+		ISelection selection = selectionProvider.getSelection();
+		colorAnnotationManager.colorXmlWriter(selection, activeEditor); 
+		
+		
+		// Create ListDialog and print the selected feature
+		listDialog.setTitle("FeatureDialog");
+		listDialog.setMessage("Feature wählen");
+		listDialog.setContentProvider(ArrayContentProvider.getInstance());
+		listDialog.setLabelProvider(new LabelProvider());
 
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			
-			// root elements
-			Document doc = docBuilder.newDocument();
-			Element rootElement = doc.createElement("root");
-			doc.appendChild(rootElement);
+		Vector<String> featureList = new Vector<String>();
 
-			// file elements
-			Element file = doc.createElement("file");
-			rootElement.appendChild(file);
-
-			// set path attribute to file element
-			Attr path = doc.createAttribute("path");
-			path.setValue("features");
-			file.setAttributeNode(path);
-
-			// set filename attribute to file element
-			Attr filename = doc.createAttribute("filename");
-			filename.setValue("Add.java");
-			file.setAttributeNode(filename);
-
-			// line elements
-			Element line = doc.createElement("line");
-			file.appendChild(line);
-
-			// set number attribute to line element
-			Attr number = doc.createAttribute("number");
-			number.setValue("5");
-			line.setAttributeNode(number);
-
-			// set feature attribute to line element
-			Attr feature = doc.createAttribute("feature");
-			feature.setValue("F1");
-			line.setAttributeNode(feature);
-
-			// shorten way
-			// staff.setAttribute("id", "1");
-
-
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory
-					.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(
-					new File(
-							"C:\\Projekt\\EclipseNew\\runtime-EclipseApplication\\Test\\ColorAnnotations2.xml"));
-
-			// Output to console for testing
-			// StreamResult result = new StreamResult(System.out);
-			transformer.transform(source, result);
-
-			// Dateien/Zeilen mit Farbinformationen abspeichern
-			System.out.println("save to file");
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		} catch (TransformerException tfe) {
-			tfe.printStackTrace();
+		IFeatureProject featureProject = null;
+		if (activeEditor != null) {
+			IFile inputFile = ((FileEditorInput) activeEditor.getEditorInput()).getFile();
+			featureProject = CorePlugin.getFeatureProject(inputFile);
 		}
+		if (featureProject != null) {
+			featureNames = featureProject.getFeatureModel().getConcreteFeatureNames();
+		}
+
+		for (String feature : featureNames) {
+			featureList.add(feature);
+		}
+
+		listDialog.setInput(featureList);
+		if (listDialog.open() == Dialog.OK) {
+			
+			System.out.println("Selected feature: " + Arrays.toString(listDialog.getResult()));
+		}
+
 	}
+
 
 	public void selectionChanged(IAction action, ISelection selection) {
 	}
@@ -115,4 +84,5 @@ public class MarkWithFeatureAction implements IEditorActionDelegate,
 			activeEditor = (ITextEditor) targetEditor;
 		}
 	}
+
 }

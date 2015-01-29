9135b9e3de132cb26c7c273f9f6e4c43c065ccb0
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-01-29 14:43:19 +0100

    Added new utility class to create line elements

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/LineElement.java
@@ -0,0 +1,21 @@
+package de.ovgu.featureide.core.cide;
+
+public class LineElement {
+	private String startLine;
+	private String endLine;
+	
+	public String getStartLine() {
+		return startLine;
+	}
+	public void setStartLine(String startLine) {
+		this.startLine = startLine;
+	}
+	public String getEndLine() {
+		return endLine;
+	}
+	public void setEndLine(String endLine) {
+		this.endLine = endLine;
+	}
+	
+	
+}

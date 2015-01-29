2ebdc34dbf7697ef0a8eae808489903953993871
Arthur Hammer <arthur.hammer@st.ovgu.de>
2015-01-29 23:39:33 +0100

    Make simple/complex Constraint tests parametrized.

--- tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/conversion/ConstraintTest.java
+++ tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/conversion/ConstraintTest.java
@@ -22,7 +22,13 @@ package de.ovgu.featureide.fm.core.conversion;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.prop4j.Implies;
 import org.prop4j.Literal;
 import org.prop4j.Node;
@@ -35,68 +41,73 @@ import de.ovgu.featureide.fm.core.Constraint;
 /**
  * Tests for class Constraint and method "isSimple". 
  * 
+ * Note: In part, tests depend on correctness and parsed Node structure of NodeReader.
+ * 
  * @author Arthur Hammer
  */
+@RunWith(Parameterized.class)
 public class ConstraintTest {
 	
-	// Note: Tests depend on correctness and Node structure of NodeReader.
-
-	@Test
-	public void testIsSimple() {
-		String[] simpleStrings = new String[]{
-				"f implies g",
-				"f implies not g",
-				"a implies b",
-				"a implies not b"};
-		
-		Literal negative = new Literal("g");
-		negative.positive = false;
-		
-		Node[] simpleNodes = new Node[] {
-				new Implies("f", "g"),
-				new Implies("f", new Not("g")),
-				new Implies("f", new Literal("g")),
-				new Implies("f", new Not(new Literal("g"))),
-				new Implies("f", negative),
-				new Implies(new Literal("f"), new Not("g")),
-				new Implies(new Literal("f"), new Literal("g")),
-				new Implies(new Literal("f"), new Not(new Literal("g")))};
-		
+	// Constraints and expected test result
+	private static final Object[] constraints = new Object[]{
+			"f implies g", true,
+			"f implies not g", true, 
+			"a implies b", true,
+			"a implies not b", true,
+			"f implies not not g", false,
+			"(not f) implies g", false,
+			"not (f implies g)", false,
+			"f iff g", false,
+			"f or g", false,
+			"f and g", false,
+			"not f", false,
+			"f", false,
+			"not f or g", false,
+			"f or not g", false,
+			"f implies (a and b and c and d)", false,
+			"true", false,
+			"false", false,
+			new Implies("f", "g"), true,
+			new Implies("f", new Not("g")), true,
+			new Implies("f", new Literal("g")), true,
+			new Implies("f", new Not(new Literal("g"))), true,
+			new Implies(new Literal("f"), new Not("g")), true,
+			new Implies(new Literal("f"), new Literal("g")), true,
+			new Implies(new Literal("f"), new Not(new Literal("g"))), true
+	};
+	
+	private Constraint constraint;
+	private boolean expected;
+	
+	public ConstraintTest(Constraint constraint, boolean expected) {
+		this.constraint = constraint;
+		this.expected = expected;
+	}
+	
+	@Parameters(name = "{0}")
+	public static Collection<Object[]> getModels() {
+		Collection<Object[]> params = new ArrayList<Object[]>();
 		NodeReader reader = new NodeReader();
 		
-		for (int i = 0; i < simpleStrings.length; i++) {
-			Constraint c = new Constraint(null, reader.stringToNode(simpleStrings[i]));
-			assertTrue(c.isSimple());
+		for (int i = 0; i < constraints.length; i += 2) {
+			if (constraints[i] instanceof String) {
+				constraints[i] = reader.stringToNode((String) constraints[i]); 
+			}
+			
+			Constraint constraint = new Constraint(null, (Node) constraints[i]);
+			params.add(new Object[]{constraint, constraints[i+1]});
 		}
 		
-		for (int i = 0; i < simpleNodes.length; i++) {
-			Constraint c = new Constraint(null, simpleNodes[i]);
-			assertTrue(c.isSimple());
-		}
+		// 1 more test case for negative literal
+		Literal negative = new Literal("g");
+		negative.positive = false;
+		params.add(new Object[]{new Constraint(null, new Implies("f", negative)), true});
+		
+		return params;
 	}
 	
 	@Test
-	public void testIsComplex() {
-		String[] complex = new String[]{
-				"f implies not not g",
-				"(not f) implies g",
-				"not (f implies g)",
-				"f iff g",
-				"f or g",
-				"f and g",
-				"not f",
-				"f",
-				"not f or g",
-				"f or not g",
-				"f implies (a and b and c and d)",
-				"true",
-				"false"};
-		
-		NodeReader reader = new NodeReader();
-		
-		for (int i = 0; i < complex.length; i++) {
-			Constraint c = new Constraint(null, reader.stringToNode(complex[i]));
-			assertFalse(c.isSimple());
-		}
+	public void testIsSimple() {
+		assertEquals(constraint.isSimple(), expected);
 	}
 }

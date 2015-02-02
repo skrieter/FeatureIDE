0b0712a0b217a63134424c067a374bbee7300107
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-02 14:06:09 +0100

    Added new action to remove annotaions from a selected feature

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/ClearFeatureAction.java
@@ -0,0 +1,55 @@
+package de.ovgu.featureide.core.cide;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class ClearFeatureAction implements IEditorActionDelegate, IViewActionDelegate {
+
+	public ITextEditor activeEditor = null;
+	ColorXmlManager colorXmlManager;
+	SelectFeatureDialog selectFeatureDialog = new SelectFeatureDialog();
+
+	public void run(IAction action) {
+		ISelectionProvider selectionProvider = activeEditor.getSelectionProvider();
+		ISelection selection = selectionProvider.getSelection();
+		ITextSelection textSelection = (ITextSelection) selection;
+		
+		Integer startLine = Integer.valueOf(textSelection.getStartLine() + 1);
+		Integer endLine = Integer.valueOf(textSelection.getEndLine() + 1);
+		
+		FileEditorInput input = (FileEditorInput) activeEditor.getEditorInput();
+		IFile file = input.getFile();
+		IProject activeProject = file.getProject();
+
+		String activeProjectPath = activeProject.getLocation().toFile().getAbsolutePath();
+		String activeProjectPathToFile = file.getLocation().toFile().getAbsolutePath();
+
+		this.colorXmlManager = new ColorXmlManager(activeProjectPath);
+
+		String feature = selectFeatureDialog.open(activeEditor);
+		this.colorXmlManager.deleteFeatureAnnotation(activeProjectPathToFile, startLine, endLine, feature);
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public void init(IViewPart view) {
+	}
+
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		if (targetEditor instanceof ITextEditor) {
+			activeEditor = (ITextEditor) targetEditor;
+		}
+
+	}
+}

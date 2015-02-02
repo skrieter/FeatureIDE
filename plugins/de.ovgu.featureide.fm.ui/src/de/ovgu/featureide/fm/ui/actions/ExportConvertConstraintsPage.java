2bf06e468ccbe5efff4f21465e1355ade04d8464
Arthur Hammer <arthur.hammer@st.ovgu.de>
2015-02-02 01:03:37 +0100

    Implemented export wizard UI and action for ConstraintConverter.

---
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/actions/ExportConvertConstraintsPage.java
@@ -0,0 +1,179 @@
+/* FeatureIDE - A Framework for Feature-Oriented Software Development
+ * Copyright (C) 2005-2013  FeatureIDE team, University of Magdeburg, Germany
+ *
+ * This file is part of FeatureIDE.
+ * 
+ * FeatureIDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FeatureIDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FeatureIDE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See http://www.fosd.de/featureide/ for further information.
+ */
+package de.ovgu.featureide.fm.ui.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+
+import de.ovgu.featureide.fm.ui.actions.ExportConvertConstraintsWizard.ConversionMethod;
+
+/**
+ * Wizard Page for exporting models using ComplexConstraintConverter.
+ * 
+ * @author Arthur Hammer
+ */
+public class ExportConvertConstraintsPage extends WizardPage{
+	
+	private static final String LABEL_METHOD = "Method:";
+	private static final String TOOLTIP_METHOD = "Which method to use when converting to a product-equivalent model without complex constraints.";
+	
+	private static final String COMBO_LABEL_CNF = "Conjunctive Normal Form (CNF) (recommended)";
+	private static final String COMBO_LABEL_CNF_NAIVE = "CNF Naive";
+	private static final String COMBO_LABEL_DNF = "Disjunctive Normal Form (DNF)";
+	private static final String COMBO_LABEL_DNF_NAIVE = "DNF Naive";
+	
+	private IFile inputModelFile;
+	private Combo comboMethod;
+	protected Text fileName;
+	protected ConversionMethod selectedMethod;
+	
+	protected ExportConvertConstraintsPage(String pageName, IFile inputFile) {
+		super(pageName);
+		setDescription(pageName);
+		this.inputModelFile = inputFile;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createControl(Composite parent) {
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		Composite composite = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		layout.verticalSpacing = 9;
+		composite.setLayout(layout);
+
+		Label labelGenerate = new Label(composite, SWT.NULL);
+		labelGenerate.setText(LABEL_METHOD);
+		labelGenerate.setToolTipText(TOOLTIP_METHOD);
+		
+		comboMethod = new Combo(composite, SWT.BORDER | SWT.SINGLE | SWT.READ_ONLY);
+		comboMethod.setLayoutData(gd);
+		comboMethod.add(COMBO_LABEL_CNF);
+		comboMethod.add(COMBO_LABEL_CNF_NAIVE);
+		comboMethod.add(COMBO_LABEL_DNF);
+		comboMethod.add(COMBO_LABEL_DNF_NAIVE);
+		comboMethod.setText(COMBO_LABEL_CNF);
+		selectedMethod = ConversionMethod.CNF;
+
+		Label fileNameLabel = new Label(composite, SWT.NULL);
+		fileNameLabel.setText("File name:");
+		
+		Composite fileComposite = new Composite(composite, SWT.NULL);
+		fileComposite.setLayout(layout);
+		
+		fileName = new Text(fileComposite, SWT.BORDER | SWT.SINGLE);
+		String modelName = inputModelFile.getLocation().removeFileExtension().toOSString();
+		fileName.setText(modelName + "-simple-constraints.xml");	
+		
+		Button browseButton = new Button(fileComposite, SWT.NONE);
+		browseButton.setText("Browse...");
+		browseButton.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
+			public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
+				String selectedPath = openFileDialog();
+				if (selectedPath != null) {
+					fileName.setText(selectedPath);
+					IPath path = new Path(selectedPath);
+					if (path.getFileExtension() == null) {
+						fileName.setText(selectedPath + ".xml");
+					}
+				}
+			}
+		});
+		
+		fileName.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				checkFileName();
+			}
+		});
+		
+		comboMethod.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				ConversionMethod[] methods = new ConversionMethod[]{ConversionMethod.CNF, ConversionMethod.CNF_NAIVE, ConversionMethod.DNF, ConversionMethod.DNF_NAIVE};
+				selectedMethod = methods[comboMethod.getSelectionIndex()];
+			}
+		});
+
+		setControl(composite);
+		checkFileName();
+	}
+	
+	protected void checkFileName() {
+		String text = fileName.getText();
+		IPath path = new Path(text);
+		if (path.isEmpty()) {
+			updateErrorMessage("File name must be specified.");
+			return;
+		}
+		if (!path.isValidPath(text)) {
+			updateErrorMessage(text + " is no valid path.");
+			return;
+		}
+		String fileExtension = path.getFileExtension();
+		if (fileExtension == null || !fileExtension.equals("xml")) {
+			updateErrorMessage("Exported model file must have xml as file extension.");
+			return;
+		}
+		if (path.toFile().exists()) {
+			updateStatusMessage("Selected file already exists. File will be overwritten.");
+			return;
+		}
+		updateErrorMessage(null);
+		updateStatusMessage(null);
+	}
+	
+	private void updateErrorMessage(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+	
+	private void updateStatusMessage(String message) {
+		setMessage(message);
+		setPageComplete(true);
+	}
+	
+	private String openFileDialog() {
+		FileDialog fileDialog = new FileDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), SWT.MULTI);
+
+		fileDialog.setFileName("simple-constraints.xml");
+		fileDialog.setFilterExtensions(new String[] { "*.xml" });
+		fileDialog.setOverwrite(true);
+		fileDialog.setFilterPath(inputModelFile.getProject().getLocation().toOSString());
+		return fileDialog.open();
+	}
+}
---
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/actions/ExportConvertConstraintsWizard.java
@@ -0,0 +1,158 @@
+/* FeatureIDE - A Framework for Feature-Oriented Software Development
+ * Copyright (C) 2005-2013  FeatureIDE team, University of Magdeburg, Germany
+ *
+ * This file is part of FeatureIDE.
+ * 
+ * FeatureIDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FeatureIDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FeatureIDE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See http://www.fosd.de/featureide/ for further information.
+ */
+package de.ovgu.featureide.fm.ui.actions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+import de.ovgu.featureide.fm.core.FeatureModel;
+import de.ovgu.featureide.fm.core.conversion.ComplexConstraintConverter;
+import de.ovgu.featureide.fm.core.conversion.ComplexConstraintConverterCNF;
+import de.ovgu.featureide.fm.core.conversion.ComplexConstraintConverterDNF;
+import de.ovgu.featureide.fm.core.io.FeatureModelReaderIFileWrapper;
+import de.ovgu.featureide.fm.core.io.UnsupportedModelException;
+import de.ovgu.featureide.fm.core.io.xml.XmlFeatureModelReader;
+import de.ovgu.featureide.fm.core.io.xml.XmlFeatureModelWriter;
+import de.ovgu.featureide.fm.ui.FMUIPlugin;
+
+
+/**
+ * Wizard for exporting models using ComplexConstraintConverter.
+ * 
+ * @author Arthur Hammer
+ */
+public class ExportConvertConstraintsWizard extends Wizard implements INewWizard {
+
+	protected static enum ConversionMethod {
+		CNF, CNF_NAIVE, DNF, DNF_NAIVE;
+	}
+
+	private IFile inputModelFile; 
+	private ExportConvertConstraintsPage page;
+	
+	public ExportConvertConstraintsWizard(IFile inputFile) {
+		this.inputModelFile = inputFile;
+	}
+
+	@Override
+	public void addPages() {
+		setWindowTitle("Export Product-Equivalent Model Without Complex Constraints");
+		page = new ExportConvertConstraintsPage("Export feature model to a product-equivalent model including only simple requires and excludes constraints.", inputModelFile);
+		addPage(page);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		final ConversionMethod method = page.selectedMethod;
+		final String outputPath = page.fileName.getText();
+		String jobName = "Exporting product-equivalent model with simple constraints (" + inputModelFile.getName() +")";
+
+		// TODO: AStoppableJob? (how to auto-cancel/kill?)	
+		Job job = new Job(jobName) {
+			protected IStatus run(IProgressMonitor monitor) {
+				FeatureModel model = readModel(inputModelFile);
+				ComplexConstraintConverter converter;
+				FeatureModel result;
+				
+				switch(method) {
+				case CNF:
+					converter = new ComplexConstraintConverterCNF();
+					result = converter.convert(model);
+					break;
+				case CNF_NAIVE:
+					converter = new ComplexConstraintConverterCNF();
+					result = converter.convertNaive(model);
+					break;
+				case DNF:
+					converter = new ComplexConstraintConverterDNF();
+					result = converter.convert(model);
+					break;
+				case DNF_NAIVE:
+					converter = new ComplexConstraintConverterDNF();
+					result = converter.convertNaive(model);
+					break;
+				default:
+					return Status.CANCEL_STATUS;
+				}
+
+				XmlFeatureModelWriter writer = new XmlFeatureModelWriter(result);
+				writer.writeToFile(new File(outputPath));
+				
+				try {
+					inputModelFile.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+				} catch (CoreException e) {
+					FMUIPlugin.getDefault().logError(e);
+				}
+				return Status.OK_STATUS;
+			}
+		};
+
+		job.setPriority(Job.INTERACTIVE);
+		job.schedule();
+		return true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		
+	}
+
+	/**
+	 * reads the featureModel from file
+	 * 
+	 * @param inputFile
+	 * @return featureModel
+	 * @throws UnsupportedModelException
+	 * @throws FileNotFoundException
+	 */
+	private FeatureModel readModel(IFile inputFile) {
+		FeatureModel fm = new FeatureModel();
+		FeatureModelReaderIFileWrapper fmReader = new FeatureModelReaderIFileWrapper(
+				new XmlFeatureModelReader(fm));
+
+		try {
+			fmReader.readFromFile(inputFile);
+		} catch (FileNotFoundException e) {
+			FMUIPlugin.getDefault().logError(e);
+		} catch (UnsupportedModelException e) {
+			FMUIPlugin.getDefault().logError(e);
+		}
+		return fm;
+	}
+}

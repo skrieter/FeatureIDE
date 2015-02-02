8e5dfcd353d303b4b0f29b8f84ebb892a024f14c
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-02 14:07:11 +0100

    Added new action to remove annotations from all features

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/ClearAllFeaturesAction.java
@@ -0,0 +1,48 @@
+package de.ovgu.featureide.core.cide;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class ClearAllFeaturesAction implements IEditorActionDelegate, IViewActionDelegate {
+
+	public ITextEditor activeEditor = null;
+	ColorXmlManager colorXmlManager;
+	SelectFeatureDialog selectFeatureDialog = new SelectFeatureDialog();
+
+	public void run(IAction action) {
+
+		FileEditorInput input = (FileEditorInput) activeEditor.getEditorInput();
+		IFile file = input.getFile();
+		IProject activeProject = file.getProject();
+
+		String activeProjectPath = activeProject.getLocation().toFile().getAbsolutePath();
+		String activeProjectPathToFile = file.getLocation().toFile().getAbsolutePath();
+
+		this.colorXmlManager = new ColorXmlManager(activeProjectPath);
+
+		this.colorXmlManager.removeAllAnnotations(activeProjectPathToFile);
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public void init(IViewPart view) {
+	}
+
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		if (targetEditor instanceof ITextEditor) {
+			activeEditor = (ITextEditor) targetEditor;
+		}
+
+	}
+}

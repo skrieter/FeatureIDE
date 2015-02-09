e1a32fed67c6725b9ccdbab250f4adc2068be0c1
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-09 14:32:27 +0100

    Added dialog and action to color the whole file

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/SetFileColorAction.java
@@ -0,0 +1,75 @@
+package de.ovgu.featureide.core.cide;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class SetFileColorAction implements IViewActionDelegate {
+
+	ColorXmlManager colorXmlManager;
+	SetFileColorDialog setFileColorDialog = new SetFileColorDialog();
+	IFile file = null;
+	String path = null;
+
+	public void run(IAction action) {
+
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+		Object firstElement = selection.getFirstElement();
+		if (firstElement instanceof IAdaptable) {
+			this.file = (IFile) ((IAdaptable) firstElement).getAdapter(IFile.class);
+			this.path = file.getLocation().toFile().getAbsolutePath();
+			System.out.println(path);
+			System.out.println(getEndline(path));
+		}
+
+		IProject activeProject = file.getProject();
+		String activeProjectPath = activeProject.getLocation().toFile().getAbsolutePath();
+		String activeProjectPathToFile = file.getLocation().toFile().getAbsolutePath();
+
+		this.colorXmlManager = new ColorXmlManager(activeProjectPath);
+
+		String feature = setFileColorDialog.open(file);
+
+		if (feature != null) {
+			this.colorXmlManager.addAnnotation(activeProjectPathToFile, 1, getEndline(path), feature);
+			while (this.colorXmlManager.mergeLines(activeProjectPathToFile, feature));
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public void init(IViewPart view) {
+	}
+	
+	//count lines for a given file
+	private int getEndline(String path) {
+		BufferedReader reader;
+		int lines = 0;
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			while (reader.readLine() != null)
+				lines++;
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lines;
+	}
+}

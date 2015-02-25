890653e1ec642276e28433adba555eacbbb4403e
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-25 09:10:49 +0100

    Renaming LineElement.java

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/core/cide/SelectionElement.java
@@ -0,0 +1,21 @@
+package de.ovgu.featureide.core.cide;
+
+public class SelectionElement {
+	private String offset;
+	private String offsetEnd;
+	
+	public String getOffset() {
+		return offset;
+	}
+	public void setOffset(String offset) {
+		this.offset = offset;
+	}
+	public String getOffsetEnd() {
+		return offsetEnd;
+	}
+	public void setOffsetEnd(String OffsetEnd) {
+		this.offsetEnd = OffsetEnd;
+	}
+	
+	
+}

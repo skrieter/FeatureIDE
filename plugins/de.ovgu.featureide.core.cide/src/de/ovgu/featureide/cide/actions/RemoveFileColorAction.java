8159db1c7fbe2dae11ad46e260040c5cbb41d412
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-26 09:31:31 +0100

    Added actions to remove colors from selected file or folder

---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/cide/actions/RemoveFileColorAction.java
@@ -0,0 +1,65 @@
+package de.ovgu.featureide.cide.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+
+import de.ovgu.featureide.cide.dialogs.SetFileColorDialog;
+import de.ovgu.featureide.core.cide.ColorXmlManager;
+
+public class RemoveFileColorAction implements IViewActionDelegate {
+
+	ColorXmlManager colorXmlManager;
+	SetFileColorDialog setFileColorDialog = new SetFileColorDialog();
+	IFile file = null;
+	String path = null;
+	IDocument doc = null;
+
+	public void run(IAction action) {
+
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (!(window.getSelectionService().getSelection() instanceof TextSelection)) {
+			IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+			Object firstElement = selection.getFirstElement();
+			if (firstElement instanceof IAdaptable) {
+				this.file = (IFile) ((IAdaptable) firstElement).getAdapter(IFile.class);
+				this.path = file.getLocation().toFile().getAbsolutePath();
+				IDocumentProvider provider = new TextFileDocumentProvider();
+				try {
+					provider.connect(file);
+				} catch (CoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				this.doc = provider.getDocument(file);
+			}
+
+			IProject activeProject = file.getProject();
+			String activeProjectPath = activeProject.getLocation().toFile().getAbsolutePath();
+			String activeProjectPathToFile = file.getLocation().toFile().getAbsolutePath();
+
+			this.colorXmlManager = new ColorXmlManager(activeProjectPath);
+
+			this.colorXmlManager.removeAllAnnotations(activeProjectPathToFile);
+
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public void init(IViewPart view) {
+	}
+}
---
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/cide/actions/RemoveFolderColorAction.java
@@ -0,0 +1,86 @@
+package de.ovgu.featureide.cide.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import de.ovgu.featureide.cide.dialogs.SetFolderColorDialog;
+import de.ovgu.featureide.core.IFeatureProject;
+import de.ovgu.featureide.core.cide.ColorXmlManager;
+
+public class RemoveFolderColorAction implements IEditorActionDelegate, IViewActionDelegate {
+
+	ColorXmlManager colorXmlManager;
+	SetFolderColorDialog setFolderColorDialog = new SetFolderColorDialog();
+	public ITextEditor activeEditor = null;
+	IFolder folder = null;
+	IResource[] folderMembers = null;
+	IFeatureProject featureProject = null;
+	IDocument doc = null;
+
+	public void run(IAction action) {
+
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+		Object firstElement = selection.getFirstElement();
+		if (firstElement instanceof IAdaptable) {
+			this.folder = (IFolder) ((IAdaptable) firstElement).getAdapter(IFolder.class);
+
+			try {
+				this.folderMembers = folder.members();
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		String activeProjectPath = folder.getProject().getLocation().toFile().getAbsolutePath();
+		this.colorXmlManager = new ColorXmlManager(activeProjectPath);
+
+		if (this.folderMembers != null) {
+			IFile file = (IFile) this.folderMembers[0];
+
+			for (int i = 0; i < this.folderMembers.length; i++) {
+				file = (IFile) this.folderMembers[i];
+				IDocumentProvider provider = new TextFileDocumentProvider();
+				try {
+					provider.connect(file);
+				} catch (CoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				this.doc = provider.getDocument(file);
+				String filePath = this.folderMembers[i].getLocation().toFile().getAbsolutePath();
+				this.colorXmlManager.removeAllAnnotations(filePath);
+
+			}
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public void init(IViewPart view) {
+	}
+
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		if (targetEditor instanceof ITextEditor) {
+			activeEditor = (ITextEditor) targetEditor;
+		}
+	}
+}

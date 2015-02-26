915fa6e469d4203a7d3a8d2377f10acadf6c8505
Kevin Roeschke <Kevin.Roeschke@metop.de>
2015-02-26 11:20:37 +0100

    Renamed variables

--- plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/cide/dialogs/UnmarkFeatureDialog.java
+++ plugins/de.ovgu.featureide.core.cide/src/de/ovgu/featureide/cide/dialogs/UnmarkFeatureDialog.java
@@ -36,9 +36,9 @@ public class UnmarkFeatureDialog {
 		ISelection selection = selectionProvider.getSelection();
 		ITextSelection textSelection = (ITextSelection) selection;
 
-		Integer startLine = Integer.valueOf(textSelection.getStartLine() + 1);
-		Integer endLine = Integer.valueOf(textSelection.getEndLine() + 1);
-
+		Integer selectedOffset = Integer.valueOf(textSelection.getOffset());
+		Integer selectedOffsetEnd = Integer.valueOf(textSelection.getLength())+Integer.valueOf(textSelection.getOffset());
+		
 		Shell shell = null;
 		Vector<String> featureList = new Vector<String>();
 		IFeatureProject featureProject = null;
@@ -52,24 +52,23 @@ public class UnmarkFeatureDialog {
 			XPathFactory xPathfactory = XPathFactory.newInstance();
 			XPath xpath = xPathfactory.newXPath();
 			try {
-				String linePathXPath = "root/files/file[@path='" + path + "']/feature/line";
+				String linePathXPath = "root/files/file[@path='" + path + "']/feature/selection";
 				XPathExpression expression = xpath.compile(linePathXPath);
-				NodeList lineNodes = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+				NodeList selectionNodes = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
 
-				for (int i = 0; i < lineNodes.getLength(); i++) {
-					Node lineNode = lineNodes.item(i);
-					Integer endlineAttribute = Integer.parseInt(lineNode.getAttributes().getNamedItem("endline").getNodeValue());
-					Integer startlineAttribute = Integer.parseInt(lineNode.getAttributes().getNamedItem("startline").getNodeValue());
+				for (int i = 0; i < selectionNodes.getLength(); i++) {
+					Node selectionNode = selectionNodes.item(i);
+					Integer offsetEndAttribute = Integer.parseInt(selectionNode.getAttributes().getNamedItem("offsetEnd").getNodeValue());
+					Integer offsetAttribute = Integer.parseInt(selectionNode.getAttributes().getNamedItem("offset").getNodeValue());
 
-					if (endlineAttribute >= endLine && startlineAttribute <= startLine) {
+					if (offsetEndAttribute >= selectedOffsetEnd && offsetAttribute <= selectedOffset) {
 
 						// get feature id and add to featurelist
-						String feature = lineNode.getParentNode().getAttributes().getNamedItem("id").getTextContent();
+						String feature = selectionNode.getParentNode().getAttributes().getNamedItem("id").getTextContent();
 						featureList.add(feature);
 					}
 				}
 			} catch (XPathExpressionException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -90,64 +89,7 @@ public class UnmarkFeatureDialog {
 				return returnFeatures;
 			}
 		}
-		// Object[] result = dialog.getResult();
 		return null;
-		
-		/*
-		ListDialog listDialog = new ListDialog(parentShel);
-		listDialog.setTitle("FeatureDialog");
-		listDialog.setMessage("Choose feature");
-		listDialog.setContentProvider(ArrayContentProvider.getInstance());
-		listDialog.setLabelProvider(new LabelProvider());
-
-		Vector<String> featureList = new Vector<String>();
-		IFeatureProject featureProject = null;
-
-		if (activeEditor != null) {
-			IFile inputFile = ((FileEditorInput) activeEditor.getEditorInput()).getFile();
-			featureProject = CorePlugin.getFeatureProject(inputFile);
-		}
-		if (featureProject != null) {
-
-			XPathFactory xPathfactory = XPathFactory.newInstance();
-			XPath xpath = xPathfactory.newXPath();
-			try {
-				String linePathXPath = "root/files/file[@path='" + path + "']/feature/line";
-				XPathExpression expression = xpath.compile(linePathXPath);
-				NodeList lineNodes = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
-
-				for (int i = 0; i < lineNodes.getLength(); i++) {
-					Node lineNode = lineNodes.item(i);
-					Integer endlineAttribute = Integer.parseInt(lineNode.getAttributes().getNamedItem("endline").getNodeValue());
-					Integer startlineAttribute = Integer.parseInt(lineNode.getAttributes().getNamedItem("startline").getNodeValue());
-
-					if (endlineAttribute >= endLine && startlineAttribute <= startLine) {
-
-						// get feature id and add to featurelist
-						String feature = lineNode.getParentNode().getAttributes().getNamedItem("id").getTextContent();
-						featureList.add(feature);
-					}
-				}
-			} catch (XPathExpressionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		// sort featurelist
-		Collections.sort(featureList);
-
-		// TODO sort FeatureList
-		listDialog.setInput(featureList);
-		if (listDialog.open() == Dialog.OK) {
-			System.out.println("Selected feature: " + Arrays.toString(listDialog.getResult()));
-			if (listDialog.getResult().length > 0) {
-				Object array[] = listDialog.getResult();
-				return (String) array[0];
-			}
-		}
-		return null;
-*/
 	}
 
 }
